70201bef2b500f945851df3c208d6d5bc303398c
Kyle <TheRealBanana@users.noreply.github.com>
2017-10-11 15:56:54 -0700

    Minor update to netspeed.py
    Got bored and started doing code reviews and found a few things to 
    change. I tried reversing the order of multis but I think it will 
    require more loop iterations rather than less on average by reordering. 
    This is explained in further detail in the comments (ln 16-19).
    
    Moved tx/rx_start to the run() function to lessen the time 
    difference between start and accum being set. There probably aren't many 
    cycles lost between object instantiation and the old tx/rx_accum variables 
    being set but it cant hurt being thorough. 
    
    Added a join() statement at line 68 just in-case these Netspeed objects 
    weren't being cleaned up properly by garbage collection.

--- netspeed.py
+++ netspeed.py
@@ -2,7 +2,7 @@
 # September 2016
 # Kyle Claisse
 __module_name__ = "Network Speed Indicator" 
-__module_version__ = "1.1" 
+__module_version__ = "1.2" 
 __module_description__ = "Prints out average network interface speed" 
 
 print "\0034" + __module_name__, __module_version__,"has been loaded\003"
@@ -13,31 +13,30 @@ from psutil import net_io_counters as psutil_net_io_counters
 from collections import OrderedDict as OD
 import threading
 
-
-#consider re-ordering this from largest to smallest. That way line 44 - 50 don't have to
-#continually set s/rx_affix and s/rx_multi. If we go from large to small it will only change once.
+#Decided against reordering. Would have had to split the check loop at lines 42-48 into
+#two separate loops. This could have theoretically improved the number of checks to just
+#two but at worst it could require 8 checks. Whereas the current implementation always
+#requires 4 checks, no more and no less. I think on average, this way is better.
 multis = OD()
 multis[1] = "Bytes/s"
 multis[1024] = "KiB/s"
 multis[1024**2] = "MiB/s"
 multis[1024**3] = "GiB/s"
+
 sample_time = 0.5 #The longer your sample time the more accurate the results will be. Value is in seconds.
 
-
 class Netspeed(threading.Thread):
-    def __init__(self, out):
-        self.out = out
-        self.tx_start = psutil_net_io_counters(pernic=False)[0]
-        self.rx_start = psutil_net_io_counters(pernic=False)[1]
+    def __init__(self, output_mode):
+        self.output_mode = output_mode
         super(Netspeed, self).__init__()
     
     def run(self):
         #Here we take two samples self.sample_time seconds apart and get an average network speed over time
-        tx_accum = 0
-        rx_accum = 0
+        tx_start = psutil_net_io_counters(pernic=False)[0]
+        rx_start = psutil_net_io_counters(pernic=False)[1]
         sleep(sample_time)
-        tx_accum += (psutil_net_io_counters(pernic=False)[0] - self.tx_start)
-        rx_accum += (psutil_net_io_counters(pernic=False)[1] - self.rx_start)
+        tx_accum = (psutil_net_io_counters(pernic=False)[0] - tx_start)
+        rx_accum = (psutil_net_io_counters(pernic=False)[1] - rx_start)
         tx_persec = float(tx_accum) / sample_time
         rx_persec = float(rx_accum) / sample_time
         #Now we figure out the multiplier and affix
@@ -53,7 +52,7 @@ class Netspeed(threading.Thread):
         tx_text = "Up: %.2f %s" % (tx_final, tx_affix)
         rx_text = "Down: %.2f %s" % (rx_final, rx_affix)
         final_output = "%s  --  %s" % (rx_text, tx_text)
-        if self.out == "say":
+        if self.output_mode == "say":
             xchat.command("SAY %s" % final_output)
         else:
             print(final_output)
@@ -62,8 +61,12 @@ class Netspeed(threading.Thread):
 def netspeed(word, word_eol, userdata):
     #Warn the user this will take some time if the sample_time is more than 1 second
     if sample_time > 1: print "Collecting network stats, this will take %.2f seconds" % sample_time
+    #Are these thread object piling up with every use??
+    #Should be be .join()'ing them? Maybe we reuse some global Netspeed object to make sure each thread always gets reused.
     ns = Netspeed(userdata)
     ns.start()
+    ns.join()
+    return xchat.EAT_ALL
     
 xchat.hook_command('netspeed', netspeed, userdata="say")
 xchat.hook_command('pnetspeed', netspeed, userdata="print")
